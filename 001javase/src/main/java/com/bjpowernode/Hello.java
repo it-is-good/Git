8b904d6cc25fb8f4bd44af0afc0127aefeb5fea9
Bin <1123399235@qq.com>
2022-10-29 20:08:28 +0800

    项目经理开发完项目提交

---
+++ 001javase/src/main/java/com/bjpowernode/Hello.java
@@ -0,0 +1,12 @@
+package com.bjpowernode;
+
+/**
+ * @author Jiang Bin
+ * @date 2022/10/29 19:56
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("这是javase的内容.......");
+    }
+}
---
+++ 002javaweb/src/main/java/com.bjpowernode/Manager.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+/**
+ * @author Jiang Bin
+ * @date 2022/10/29 19:59
+ */
+public class Manager {
+    public static void main(String[] args) {
+        System.out.println("这是javaweb项目。。。");
+    }
+}
