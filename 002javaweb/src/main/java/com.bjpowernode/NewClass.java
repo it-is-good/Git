85d9c71bc05c26d310d944ac9b904b0d336a14c5
Bin <1123399235@qq.com>
2022-10-29 20:25:44 +0800

    项目经理推送后在javaweb增加新类NewClass

---
+++ 002javaweb/src/main/java/com.bjpowernode/NewClass.java
@@ -0,0 +1,11 @@
+package com.bjpowernode;
+
+/**
+ * @author Jiang Bin
+ * @date 2022/10/29 20:24
+ */
+public class NewClass {
+    public static void main(String[] args) {
+        System.out.println("这是项目经理推送完项目增加的类");
+    }
+}
